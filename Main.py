36d77da5d4db6365f2cd6537dd4c9966f9802517
Ben-Vollrath <ben.m.vollrath@gmail.com>
2023-05-12 22:57:07 +0200

    Added Data Fetch for Hours of Day

--- Main.py
+++ Main.py
@@ -12,16 +12,108 @@ def sendRequest(start_date,end_date):
         if(start_date == None or start_date == '' or end_date == '' or end_date == None):
             r = requests.get("http://localhost:8080/patientArrival") #Get all Data
         else:
-            dt_obj = datetime.strptime(start_date, '%d.%m.%Y %H:%M:%S')#Time in Milis
+            dt_obj = datetime.strptime(start_date, '%d.%m.%Y')#Time in Milis
             mili_start_date = int(dt_obj.timestamp() * 1000)
 
-            dt_obj = datetime.strptime(end_date, '%d.%m.%Y %H:%M:%S')#Time in Milis
+            dt_obj = datetime.strptime(end_date, '%d.%m.%Y')#Time in Milis
             mili_end_date = int(dt_obj.timestamp() * 1000)
 
             r = requests.get("http://localhost:8080/patientArrival?from=" + str(mili_start_date) + "&to=" + str(mili_end_date)) #Get Data in TimeFrame
 
             
         return r.json()# Return all Data as Json
+
+def getDataOfDay(json_data, day, hospitalID):#Gets the Amount of Transports on a given weekday as average ordered by hour of day
+    
+
+    #Get the amount of times given week day has occured in time -> used to calculate average value later
+    start_date = currentMillisToDateTime(json_data[0]['created'])
+    end_date = currentMillisToDateTime(json_data[-1]['created'])
+    countOfDays = [0] * 7
+    for weekday in range(0,7):
+        countOfDays[weekday] = count_weekdays(start_date,end_date,weekday)
+    countOfDay = countOfDays[day]
+    print(str(countOfDay) + "s")
+    
+    hours_data_list = [0] * 24 #used to store amount of drives in given hour frame
+    for item in json_data:
+        milliTime = item['created']
+        dateTime = currentMillisToDateTime(milliTime)
+        weekDay = dateTime.weekday()
+
+
+        if(weekDay==day and ( hospitalID == None or hospitalID == '' or item['hospitalId'] == int(hospitalID) ) ): # We only need Data from the Weekday we are looking at and correct hospitalID
+            hour = dateTime.hour
+            hours_data_list[hour] += 1
+        #Get the average by dividing through amount of days    
+
+    data = {}
+
+    sonstige = [0] * 24
+
+    for hour in range(0,24):
+        hours_data_list[hour] = hours_data_list[hour] / countOfDay if countOfDay > 0 else hours_data_list[hour]
+
+        biggestDepartments = hourlyCountDepartments(weekday,hour, countOfDay,json_data,hospitalID)
+
+        departmentsum = biggestDepartments[0][1] + biggestDepartments[1][1] + biggestDepartments[2][1] 
+        
+        sonstige = hours_data_list[hour] - departmentsum
+
+        
+        data[hour] = [["Sonstige ", sonstige],
+                        ["Abteilung " + str(biggestDepartments[0][0]), biggestDepartments[0][1]],
+                          ["Abteilung " + str(biggestDepartments[1][0]), biggestDepartments[1][1]],
+                           ["Abteilung " + str(biggestDepartments[2][0]), biggestDepartments[2][1]]]
+    
+
+
+def hourlyCountDepartments(weekDay,hour,countOfDay,transportData,hospitalID):
+    r = requests.get("http://localhost:8080/departmentCategory") #Get all Data
+    json = r.json()#return all data as Jason
+
+    usedDepartmenArr = [0] * (len(json)+1)
+
+    
+    for item in transportData:
+
+        milliTime = item['created']
+        dateTime = currentMillisToDateTime(milliTime)  #get the weekday from item
+        weekDayFromItem = dateTime.weekday()
+        hourFromItem = dateTime.hour
+        
+        departmentCata = item['departmentCategory']
+        if departmentCata != None and weekDayFromItem == weekDay and hourFromItem == hour:
+            if(hospitalID == None or hospitalID == '' or item['hospitalId'] == int(hospitalID)):
+                    
+                id_value = departmentCata['id']
+                usedDepartmenArr[id_value]+=1
+    listClone= usedDepartmenArr.copy()
+        
+    usedDepartmenArr.sort(reverse=True)
+    listClone.index(usedDepartmenArr[0])
+
+    dp0, dp1, dp2 = None, None, None
+
+    for item in transportData:
+        department = item['departmentCategory']
+        if(department == None):
+            continue
+
+        if(listClone.index(usedDepartmenArr[0]) == department['id']):
+            dp0 = department['name']
+        if(listClone.index(usedDepartmenArr[1]) == department['id']):
+            dp1 = department['name']
+        if(listClone.index(usedDepartmenArr[2]) == department['id']):
+            dp2 = department['name']
+
+    out0 = (dp0, usedDepartmenArr[0] / countOfDay) if dp0 is not None else ("No department", 0)
+    out1 = (dp1, usedDepartmenArr[1] / countOfDay) if dp1 is not None else ("No department", 0)
+    out2 = (dp2, usedDepartmenArr[2] / countOfDay) if dp2 is not None else ("No department", 0)
+
+
+    return [out0, out1, out2]
+
 
 
 
@@ -53,12 +145,12 @@ def createSum(json_data,average_list,hospitalID):#Creates the average of the sum
 
     data = {}
 
-    sonstige = [0] * 7 
+    sonstige = [0] * 7  
 
     #Calculate the average of trips ordered by day, save the averages in data dictionary
     for weekday in range(0,7):
         biggestDepartments = count_departments(weekday,countOfDays[weekday],json_data,hospitalID)
-        average_list[weekday] = average_list[weekday] / countOfDays[weekday] if countOfDays[weekday] > 0 else 1
+        average_list[weekday] = average_list[weekday] / countOfDays[weekday] if countOfDays[weekday] > 0 else average_list[weekDay]
         departmentsum = biggestDepartments[0][1] + biggestDepartments[1][1] + biggestDepartments[2][1] 
         
         sonstige[weekday] = average_list[weekday] - departmentsum
@@ -165,7 +257,7 @@ def plot(data):
 
 
 app.layout = html.Div([
-    html.H1('Interactive Graph'),
+    html.H1('Interaktives Datendarstellungsmodel zur Datebankintegrierten Datenauferbeitung'),
     dcc.Graph(id='graph'),
     html.Label('Start Date'),
     dcc.Input(id='start-date', type='text', value=''),
@@ -194,8 +286,6 @@ def update_graph(n_clicks, start_date, end_date, hospital_id):
     return go.Figure()
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
-    
+    #app.run_server(debug=True)
+    print("No")
 
